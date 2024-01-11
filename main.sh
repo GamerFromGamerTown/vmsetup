41aed3502b1443540630f7249078dff69f21e754
GamerFromGamerTown <100511215+GamerFromGamerTown@users.noreply.github.com>
2024-01-10 21:27:44 -0900

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,33 @@
+# Update package list
+sudo apt update
+
+# Install necessary tools
+sudo apt install curl
+
+# Add Brave browser repository and key
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Update package list again after adding new repository
+sudo apt update
+
+# Install Brave browser, Sway, PCManFM, Alacritty, Ly, Rofi, and Neofetch
+sudo apt install brave-browser sway pcmanfm alacritty ly rofi neofetch
+
+# Configure Sway for user 'vm'
+VM_HOME=$(getent passwd "vm" | cut -d: -f6)
+mkdir -p "$VM_HOME/.config/sway"
+cp /etc/sway/config "$VM_HOME/.config/sway/config"
+echo "exec brave-browser" >> "$VM_HOME/.config/sway/config"
+echo 'bindsym $mod+r exec rofi -show run' >> "$VM_HOME/.config/sway/config"
+chown -R vm:vm "$VM_HOME/.config/sway"
+
+# Configure Ly for autologin
+sed -i 's/# auto_login = false/auto_login = true/' /etc/ly/config.ini
+sed -i 's/# auto_username =/auto_username = vm/' /etc/ly/config.ini
+sed -i 's/# auto_wayland = sway/auto_wayland = sway/' /etc/ly/config.ini
+
+# Enable Ly display manager
+sudo systemctl enable ly
+# make windows + r = rofi (app launcher)
+echo 'bindsym $mod+r exec rofi -show run' >> ~/.config/sway/config
